003e39fa8a168e064c848dd9b008efe86c6d0905
NateChenXJ <natechenfly@gmail.com>
2017-03-24 17:46:14 -0600

    Upload Calcu
    Final Lab6

---
+++ app/src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,70 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.util.Log;
+
+
+import java.util.regex.Pattern;
+
+public class Calculator extends AppCompatActivity {
+
+    private TextView screen;
+    private String display = "";
+    private String currentOperator = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        screen = (TextView)findViewById(R.id.textView);
+        screen.setText(display);
+    }
+
+    private void updateScreen(){
+
+        screen.setText(display);
+
+    }
+
+    public void onClickNumber(View v){
+
+        Button b = (Button) v;
+        display += b.getText();
+        updateScreen();
+
+    }
+
+    public void onClickOperator(View v){
+
+        Button b = (Button) v;
+        display += b.getText();
+        currentOperator = b.getText().toString();
+        updateScreen();
+
+    }
+
+    private void clear(){
+
+        display = "";
+        currentOperator = "";
+
+    }
+
+    public void onClickClear(View v){
+
+        clear();
+        updateScreen();
+
+    }
+
+
+
+
+
+
+}
